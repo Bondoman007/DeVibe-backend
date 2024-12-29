ac5d5c7a831f944b4e18a10206c09e88490e03e9
Bondoman007 <kanishkshrivastava037@gmail.com>
2024-12-30 02:53:02 +0530

    multiple route handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const app = express()
  
-app.use("/test",(req,res)=>{
-    res.send('hello testing')
+app.get("/test",(req,res,next)=>{
+    next()
+},(req,res,next)=>{
+    res.send(' testing')
 })
-app.get("/id/:userId/:name/:password",(req,res)=>{
+app.get("/id/:userId/:name/:password",(req,res,next)=>{
     console.log(req.params)
     res.send('id verified')
-})
+},)
 
 app.get("/test/:")
 app.listen(3000, () => {
     console.log("server is listening port 3000")
 })
-
-
-
-
