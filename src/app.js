37b05c45a5761193e384ff7e6cfcfb1c96233075
Bondoman007 <kanishkshrivastava037@gmail.com>
2024-12-29 18:47:18 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ const app = express()
 app.use("/test",(req,res)=>{
     res.send('hello testing')
 })
-app.get("/id",(req,res)=>{
+app.get("/id/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
     res.send('id verified')
 })
+
+app.get("/test/:")
 app.listen(3000, () => {
     console.log("server is listening port 3000")
 })
