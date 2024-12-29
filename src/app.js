89889025474dd681044f186e2c8f29d6dff6471c
Bondoman007 <kanishkshrivastava037@gmail.com>
2024-12-29 18:19:06 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ const app = express()
 app.use("/test",(req,res)=>{
     res.send('hello testing')
 })
-
+app.get("/id",(req,res)=>{
+    res.send('id verified')
+})
 app.listen(3000, () => {
     console.log("server is listening port 3000")
 })
+
+
+
+
