f1782eb9557b0705eb80d976c706a1e3b304f7f5
Bondoman007 <kanishkshrivastava037@gmail.com>
2025-01-19 20:17:07 +0530

    building feed api and applying pagination

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -10,6 +10,8 @@ const connectionRequestSchema = new mongoose.Schema(
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref: "User"
+
     },
     status: {
         type: String,
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express  = require("express")
 const { userAuth } = require("../middleware/auth")
 const router = express.Router()
 const   ConnectionRequest = require("../models/connectionRequest")
+const User = require("../models/user")
 router.get("/user/request",userAuth, async (req,res)=>{
    try{ const loggedInUser = req.user
     const request = await ConnectionRequest.find({
@@ -9,6 +10,9 @@ router.get("/user/request",userAuth, async (req,res)=>{
         status: "interested"
     }).populate(
         "fromUserId",
+        "firstName lastName photoUrl age gender about skills"
+    ).populate(
+        "toUserId",
         "firstName lastName photoUrl age gender about skills"
     )
     if(!request){
@@ -40,7 +44,12 @@ router.get("/user/connections",userAuth, async (req,res)=>{
         if(!connections){
            throw new Error("No connections found")
         }
-        const data = connections.map((row)=>row.fromUserId)
+        const data = connections.map((row)=>{
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId
+            }
+            return row.fromUserId
+        })
         res.json({
             message: "connections:",
             data: data
@@ -50,7 +59,45 @@ router.get("/user/connections",userAuth, async (req,res)=>{
     }
 })
 
-router.get("/user/feed",userAuth, async (req,res)=>{})
+router.get("/user/feed",userAuth, async (req,res)=>{
+  try { 
+    const loggedInUser = req.user
+    const page = parseInt(req.query.page) || 1
+    let limit = parseInt(req.query.limit) || 10
+    limit - limit > 50 ? 50 : limit
+    const skip = (page-1) * limit
+    const connections = await ConnectionRequest.find({
+        $or : [{
+           fromUserId : loggedInUser._id,
+        },{
+           toUserId : loggedInUser._id
+       }]
+
+   })
+   const hideUsers = new Set()
+   connections.forEach((req)=>{
+    hideUsers.add(req.fromUserId.toString())
+    hideUsers.add(req.toUserId.toString())
+   })
+   const users = await User.find({
+    $and: [
+        //id is not in the set hideUser
+        {_id: {$nin: Array.from(hideUsers)}},
+        //id is not equall loggedin user
+        {_id: {$ne: loggedInUser._id}}
+    ]
+   }).select( "firstName lastName photoUrl age gender about skills")
+   .skip(skip)
+   .limit(limit)
+   
+    res.json({
+        message: "feed",
+        data: users
+    })
+    }catch(err){
+     res.status(400).send("ERROR:"+err.message) 
+    }
+})
 
 
 module.exports = router
